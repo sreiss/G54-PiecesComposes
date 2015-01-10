f1f697241652aec7a2a6ad668085ceca358b242b
sreiss <simon.reiss57@gmail.com>
2015-01-10 14:24:45 +0100

    Float parse errors handled

--- src/piecec/view/IHMGestionnaireStock.java
+++ src/piecec/view/IHMGestionnaireStock.java
@@ -5,6 +5,7 @@ import piecec.controller.GestionnairePieces;
 import piecec.model.Piece;
 import piecec.model.PieceBase;
 import piecec.model.PieceComposite;
+import sun.misc.FloatingDecimal;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -40,12 +41,17 @@ public class IHMGestionnaireStock extends Vue {
         String numidS = this.readInput("-1");
         int numid = Integer.parseInt(numidS);
         Piece p = this.gestionnairePieces.getPieceById(numid);
-        System.out.println(p.toString());
-        if (p instanceof PieceComposite) {
-            System.out.println("composée de :");
-            for (Piece sp : ((PieceComposite) p).getPieces()) {
-                System.out.println(sp.getNom());
+
+        if (p != null) {
+            System.out.println(p.toString());
+            if (p instanceof PieceComposite) {
+                System.out.println("composée de :");
+                for (Piece sp : ((PieceComposite) p).getPieces()) {
+                    System.out.println(sp);
+                }
             }
+        } else {
+            System.out.println("La piece n'existe pas !");
         }
     }
 
@@ -62,14 +68,19 @@ public class IHMGestionnaireStock extends Vue {
 
         System.out.println("Veuillez entrer le prix d'achat de la piece (ex: 2.0) en €");
         String prixAchatS = this.readInput("0");
-        float prixAchat = Float.parseFloat(prixAchatS);
+        try {
+            float prixAchat = Float.parseFloat(prixAchatS);
 
-        int numid = this.gestionnairePieces.addPieceBase(nom, prixAchat);
+            int numid = this.gestionnairePieces.addPieceBase(nom, prixAchat);
 
-        if (numid < 0) {
-            System.out.println("ERREUR : la piece n'a pas pu être ajouté.");
-        } else {
-            System.out.println(MessageFormat.format("La pièce a bien été ajoutée, elle porte le numid {0}", numid));
+            if (numid < 0) {
+                System.out.println("ERREUR : la piece n'a pas pu être ajouté.");
+            } else {
+                System.out.println(MessageFormat.format("La pièce a bien été ajoutée, elle porte le numid {0}", numid));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Les prix des pieces doivent être au format suivant : 1.0, vous avez probablement mis une virgule.");
+            System.out.println("Veuillez réessayer d'ajouter la piece.");
         }
     }
 
@@ -79,26 +90,31 @@ public class IHMGestionnaireStock extends Vue {
 
         System.out.println("Veuillez entrer le cout d'assemblace de la piece (ex: 2.0) en €");
         String coutAssemblageS = this.readInput("0");
-        float coutAssemblage = Float.parseFloat(coutAssemblageS);
+        try {
+            float coutAssemblage = Float.parseFloat(coutAssemblageS);
 
-        System.out.println("Veuillez entrer le numid des pièces qui compose celle-ci (séparées par une virgule)");
-        String piecesS = this.readInput("");
-        String[] pieceNumidsArray;
-        ArrayList<Integer> pieceNumids = new ArrayList<Integer>();
-        List<Piece> pieces = new ArrayList<Piece>();
-        if (piecesS.length() > 0) {
-            pieceNumidsArray = piecesS.split(",");
-            for (String pieceS : pieceNumidsArray) {
-                pieceNumids.add(Integer.parseInt(pieceS));
+            System.out.println("Veuillez entrer le numid des pièces qui compose celle-ci (séparées par une virgule)");
+            String piecesS = this.readInput("");
+            String[] pieceNumidsArray;
+            ArrayList<Integer> pieceNumids = new ArrayList<Integer>();
+            List<Piece> pieces = new ArrayList<Piece>();
+            if (piecesS.length() > 0) {
+                pieceNumidsArray = piecesS.split(",");
+                for (String pieceS : pieceNumidsArray) {
+                    pieceNumids.add(Integer.parseInt(pieceS));
+                }
             }
-        }
 
-        int numid = this.gestionnairePieces.addPieceComposite(nom, coutAssemblage, pieceNumids);
+            int numid = this.gestionnairePieces.addPieceComposite(nom, coutAssemblage, pieceNumids);
 
-        if (numid < 0) {
-            System.out.println("ERREUR : la piece n'a pas pu être ajouté.");
-        } else {
-            System.out.println(MessageFormat.format("Pièce ajoutée, numid : {0}", numid));
+            if (numid < 0) {
+                System.out.println("ERREUR : la piece n'a pas pu être ajouté.");
+            } else {
+                System.out.println(MessageFormat.format("Pièce ajoutée, numid : {0}", numid));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Les prix des pieces doivent être au format suivant : 1.0, vous avez probablement mis une virgule.");
+            System.out.println("Veuillez réessayer d'ajouter la piece.");
         }
     }
 }
